de60e2c2a2c3b03be95f00dd539a3ff879c4e4aa
pruisner1 <54373319+pruisner1@users.noreply.github.com>
2019-08-22 14:14:32 -0500

    Add project files.

---
+++ MyFirstProject/Main.cpp
@@ -0,0 +1,16 @@
+/*	Lane Pruisner
+	C++ 2019
+	Due Date:August 29th
+	Lab 1 Introduction to C++ programming
+	Learning how to create a program and output "Hello World!"
+*/
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!";
+
+	return 0;
+}
